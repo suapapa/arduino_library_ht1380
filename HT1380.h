d26eb68a4352823cda006f565fda7920cede992a
Homin Lee <homin.lee@suapapa.net>
2012-06-12 16:51:25 +0900

    Cleaned up indentation

--- HT1380.h
+++ HT1380.h
@@ -13,75 +13,74 @@
 
 #ifndef HT1380_H
 #define HT1380_H
-class HT1380
-{
-	public:
-		HT1380(uint8_t rest, uint8_t io, uint8_t sclk);
+class HT1380 {
+public:
+    HT1380(uint8_t rest, uint8_t io, uint8_t sclk);
 
-		void init(void);
+    void init(void);
 
-		void readHour(void);
-		void readMin(void);
-		void readSec(void);
-		void readYear(void);
-		void readMonth(void);
-		void readDate(void);
-		void readDay(void);
-		void readWP(void);
-		void readBurst(void);
+    void readHour(void);
+    void readMin(void);
+    void readSec(void);
+    void readYear(void);
+    void readMonth(void);
+    void readDate(void);
+    void readDay(void);
+    void readWP(void);
+    void readBurst(void);
 
-		void writeHour(void);
-		void writeMin(void);
-		void writeSec(void);
-		void writeYear(void);
-		void writeMonth(void);
-		void writeDate(void);
-		void writeDay(void);
-		void writeWP(void);
-		void writeBurst(void);
+    void writeHour(void);
+    void writeMin(void);
+    void writeSec(void);
+    void writeYear(void);
+    void writeMonth(void);
+    void writeDate(void);
+    void writeDay(void);
+    void writeWP(void);
+    void writeBurst(void);
 
-		uint8_t getHour(void);
-		uint8_t getMin(void);
-		uint8_t getSec(void);
-		uint8_t getYear(void);
-		uint8_t getMonth(void);
-		uint8_t getDate(void);
-		uint8_t getDay(void);
-		uint8_t getWP(void);
+    uint8_t getHour(void);
+    uint8_t getMin(void);
+    uint8_t getSec(void);
+    uint8_t getYear(void);
+    uint8_t getMonth(void);
+    uint8_t getDate(void);
+    uint8_t getDay(void);
+    uint8_t getWP(void);
 
-		void setHour(uint8_t hour);
-		void setMin(uint8_t min);
-		void setSec(uint8_t sec);
-		void setYear(uint8_t year);
-		void setMonth(uint8_t month);
-		void setDate(uint8_t date);
-		void setDay(uint8_t day);
-		void setWP(uint8_t wp);
+    void setHour(uint8_t hour);
+    void setMin(uint8_t min);
+    void setSec(uint8_t sec);
+    void setYear(uint8_t year);
+    void setMonth(uint8_t month);
+    void setDate(uint8_t date);
+    void setDay(uint8_t day);
+    void setWP(uint8_t wp);
 
-	private:
-		uint8_t _pinSCLK;
-		uint8_t _pinIO;
-		uint8_t _pinREST;
+private:
+    uint8_t _pinSCLK;
+    uint8_t _pinIO;
+    uint8_t _pinREST;
 
-		uint8_t _hour;
-		uint8_t _min;
-		uint8_t _sec;
-		uint8_t _year;
-		uint8_t _month;
-		uint8_t _date;
-		uint8_t _day;
-		uint8_t _wp;
+    uint8_t _hour;
+    uint8_t _min;
+    uint8_t _sec;
+    uint8_t _year;
+    uint8_t _month;
+    uint8_t _date;
+    uint8_t _day;
+    uint8_t _wp;
 
-		uint8_t	_readByte(void);
-		void	_writeByte(uint8_t inByte);
-		void	_ioStart(void);
-		void	_ioEnd(void);
+    uint8_t	_readByte(void);
+    void	_writeByte(uint8_t inByte);
+    void	_ioStart(void);
+    void	_ioEnd(void);
 
-		uint8_t	_readRegistor(uint8_t regCmd);
-		void	_writeRegistor(uint8_t regCmd, uint8_t regValue);
+    uint8_t	_readRegistor(uint8_t regCmd);
+    void	_writeRegistor(uint8_t regCmd, uint8_t regValue);
 
-		uint8_t _parseReg2Num(uint8_t inReg);
-		uint8_t _parseNum2Reg(uint8_t inNum);
+    uint8_t _parseReg2Num(uint8_t inReg);
+    uint8_t _parseNum2Reg(uint8_t inNum);
 };
 
 #endif
